981ee2c5c882447fa44b6a63384e5f07fc2d1efb
marianarodlm <98848408+marianarodlm@users.noreply.github.com>
2022-09-13 09:29:39 -0500

    Add files via upload

---
+++ parkinglot.py
@@ -0,0 +1,16 @@
+class cars:
+    def __init__(self, compacto:str, suv: str, van: str) -> None:
+        self.compacto = compacto
+        self.suv = suv
+        self.van = van
+
+        def horas():
+            def cobro():    
+                pass
+
+class aparcarmiento:
+    def __init__(self, compacto:str, suv: str, van: str) -> None:
+        self.compacto = compacto
+        self.suv = suv
+        self.van = van
+        pass
